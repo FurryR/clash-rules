9f7250511eb14627f77c38fa40325b8d275ffe76
FurryR <awathefox@gmail.com>
2023-09-08 23:26:29 +0800

    :sparkles: URLTest & Fallback

--- doc/CONFIG.md
+++ doc/CONFIG.md
@@ -5,26 +5,31 @@
 - [配置](#配置)
   - [rules](#rules)
     - [Rule](#rule)
+    - [URLTest](#urltest)
+    - [Fallback](#fallback)
   - [override](#override)
 
 ## rules
 
-`Rule[]`：规则列表。
+`(Rule | URLTest | Fallback)[]`：规则列表。
 
 ### Rule
 
 ```typescript
 interface Rule {
-  rule: URL | string
+  type: 'select'
+  rule?: URL | string
   name: string
   behavior?: 'classical' | 'ipcidr' | 'domain'
   interval?: number
   proxy?: string[]
+  preferred?: string[]
   reuse?: boolean
 }
 ```
 
-1. url：在线的 URL 路径，或者内联的单条规则。为空时不实际导入规则集，只生成代理集。当需要指定本地文件时，请使用 `file` 协议头，并且只允许使用相对路径（Clash home directory 作为基准路径）。
+1. type：必须指定为 `select`，用于区分多个类型。
+2. rule：在线的 URL 路径，或者内联的单条规则。为空时不实际导入规则集，只生成代理集。当需要指定本地文件时，请使用 `file` 协议头，并且只允许使用相对路径（Clash home directory 作为基准路径）。
 
    - 内联规则格式如下所示：
 
@@ -40,11 +45,58 @@ interface Rule {
    MATCH,🐟 漏网之鱼
    ```
 
-2. name: 用户可读的名称。
-3. behavior：控制配置解释的行为。默认为 `classical`。
-4. interval：clash 更新配置的循环周期。默认为 86400。
-5. proxy：指定代理集对应的 `proxy` 列表。不指定时默认为原配置的全部代理/代理集。
-6. reuse：指定该代理集同时可以被其它生成的代理集使用。这意味着其它的代理集将把此代理集作为可选代理。默认为 false。
+3. name：用户可读的名称。
+4. behavior：控制配置解释的行为。默认为 `classical`。
+5. interval：clash 更新配置的循环周期。默认为 `86400`。
+6. proxy：指定代理集对应的 `proxy` 列表。不指定时默认为原配置的全部代理/代理集。
+7. preferred：指定的代理将被按顺序优先加入到代理集中，而不管它是否存在。
+8. reuse：指定该代理集同时可以被其它生成的代理集使用。这意味着其它的代理集将把此代理集作为可选代理。默认为 `false`。
+
+### URLTest
+
+```typescript
+interface URLTest {
+  type: 'url-test'
+  url: URL
+  name: string
+  interval: number
+  tolerance: number
+  proxy?: string[]
+  preferred?: string[]
+  reuse?: boolean
+}
+```
+
+1. type：必须指定为 `url-test`，用于区分多个类型。
+2. url：URL 测试使用的 URL。
+3. name：在 proxy-groups 中显示的名称。
+4. interval：URL 测试间隔。
+5. tolerance：允许的延迟偏差值。
+6. proxy：要求 url-test 在指定的代理列表中做出选择。
+7. preferred：指定的代理将被按顺序优先加入到代理集中，而不管它是否存在。
+8. reuse：指定该代理集同时可以被其它生成的代理集使用。这意味着其它的代理集将把此代理集作为可选代理。默认为 `false`。
+
+### Fallback
+
+```typescript
+interface Fallback {
+  type: 'fallback'
+  url: URL
+  name: string
+  interval: number
+  proxy?: string[]
+  preferred?: string[]
+  reuse?: boolean
+}
+```
+
+1. type：必须指定为 `fallback`，用于区分多个类型。
+2. url：URL 测试使用的 URL。
+3. name：在 proxy-groups 中显示的名称。
+4. interval：URL 测试间隔。
+5. proxy：要求 fallback 在指定的代理列表中做出选择。
+6. preferred：指定的代理将被按顺序优先加入到代理集中，而不管它是否存在。
+7. reuse：指定该代理集同时可以被其它生成的代理集使用。这意味着其它的代理集将把此代理集作为可选代理。默认为 `false`。
 
 ## override
 
--- src/main.cjs
+++ src/main.cjs
@@ -1,13 +1,19 @@
 // @ts-check
 /**
- * @typedef {{rule: URL | string, name: string, behavior?: string, interval?: number, proxy?: string[], reuse?: boolean}} Rule
+ * @typedef {{type: 'select', rule?: URL | string, name: string, behavior?: string, interval?: number, proxy?: string[], preferred?: string[], reuse?: boolean}} Rule
  */
 /**
- * @typedef {Rule[]} RuleSet
+ * @typedef {{type: 'url-test', url: URL, name: string, interval: number, tolerance: number, proxy?: string[], preferred?: string[], reuse?: boolean}} URLTest
+ */
+/**
+ * @typedef {{type: 'fallback', url: URL, name: string, interval: number, proxy?: string[], preferred?: string[], reuse?: boolean}} Fallback
+ */
+/**
+ * @typedef {(Rule | URLTest | Fallback)[]} RuleSet
  */
 module.exports.parse = ({ content, name }) => {
   if (!globalThis.config)
-    console.error('未指定 config，clash-rules 将不会生效。')
+    console.error('[clash-rules] 未指定 config，clash-rules 将不会生效。')
   /**
    * @type {RuleSet}
    */
@@ -39,44 +45,99 @@ module.exports.parse = ({ content, name }) => {
   if (useable_proxy.length == 0) useable_proxy = ['DIRECT', 'REJECT']
   // 开始添加代理
   for (const rule of rules) {
-    if (
-      content['proxy-groups'].every((value) => {
-        return value.name != rule.name
-      })
-    ) {
-      content['proxy-groups'].push({
-        name: rule.name,
-        type: 'select',
-        proxies: rule.proxy ?? useable_proxy,
-      })
-    }
-    if (rule.rule instanceof URL) {
-      if (rule.rule.protocol == 'file:') {
-        console.log(
-          `[clash-rules] 正在添加 ${rule.name}(本地文件, ${rule.rule.pathname})`,
-        )
-        content['rule-providers'][`clash-rules-${id}`] = {
-          behavior: rule.behavior ?? 'classical',
-          interval: rule.interval ?? 86400,
-          path: rule.rule.pathname,
-          type: 'file',
-        }
-      } else {
-        console.log(
-          `[clash-rules] 正在添加 ${rule.name}(URL, ${rule.rule.toString()})`,
-        )
-        content['rule-providers'][`clash-rules-${id}`] = {
-          behavior: rule.behavior ?? 'classical',
-          interval: rule.interval ?? 86400,
-          url: rule.rule.toString(),
-          type: 'http',
+    /** @type {string[]} */
+    let final_order = []
+    if (rule.preferred) {
+      final_order = rule.preferred
+      for (const v of rule.proxy ?? useable_proxy) {
+        if (!final_order.includes(v)) final_order.push(v)
+      }
+    } else final_order = rule.proxy ?? useable_proxy
+    if (rule.type == 'select') {
+      if (
+        content['proxy-groups'].every((value) => {
+          return value.name != rule.name
+        })
+      ) {
+        content['proxy-groups'].push({
+          name: rule.name,
+          type: 'select',
+          proxies: final_order,
+        })
+      }
+      if (rule.rule) {
+        if (rule.rule instanceof URL) {
+          if (rule.rule.protocol == 'file:') {
+            console.log(
+              `[clash-rules] 正在添加 ${rule.name}(本地文件, ${rule.rule.pathname})`,
+            )
+            content['rule-providers'][`clash-rules-${id}`] = {
+              behavior: rule.behavior ?? 'classical',
+              interval: rule.interval ?? 86400,
+              path: rule.rule.pathname,
+              type: 'file',
+            }
+          } else {
+            console.log(
+              `[clash-rules] 正在添加 ${rule.name}(URL, ${rule.rule})`,
+            )
+            content['rule-providers'][`clash-rules-${id}`] = {
+              behavior: rule.behavior ?? 'classical',
+              interval: rule.interval ?? 86400,
+              url: rule.rule.toString(),
+              type: 'http',
+            }
+          }
+          content['rules'].push(`RULE-SET,clash-rules-${id},${rule.name}`)
+          id++
+        } else {
+          console.log(`[clash-rules] 正在添加 ${rule.name}(内联)`)
+          content['rules'].push(`${rule.rule},${name}`)
         }
       }
-      content['rules'].push(`RULE-SET,clash-rules-${id},${rule.name}`)
-      id++
-    } else {
-      console.log(`[clash-rules] 正在添加 ${rule.name}(内联)`)
-      content['rules'].push(`${rule.rule},${name}`)
+    } else if (rule.type == 'url-test') {
+      if (
+        content['proxy-groups'].every((value) => {
+          return value.name != rule.name
+        })
+      ) {
+        console.log(
+          `[clash-rules] 正在添加 ${rule.name}(url-test, ${rule.url})`,
+        )
+        content['proxy-groups'].push({
+          name: rule.name,
+          type: 'url-test',
+          url: rule.url.toString(),
+          interval: rule.interval,
+          tolerance: rule.tolerance,
+          proxies: final_order,
+        })
+      } else {
+        console.error(
+          `[clash-rules] 检测到名字重复(url-test, ${rule.name})，已忽略更新的代理组，这可能导致非预期结果。`,
+        )
+      }
+    } else if (rule.type == 'fallback') {
+      if (
+        content['proxy-groups'].every((value) => {
+          return value.name != rule.name
+        })
+      ) {
+        console.log(
+          `[clash-rules] 正在添加 ${rule.name}(fallback, ${rule.url})`,
+        )
+        content['proxy-groups'].push({
+          name: rule.name,
+          type: 'fallback',
+          url: rule.url.toString(),
+          interval: rule.interval,
+          proxies: final_order,
+        })
+      } else {
+        console.error(
+          `[clash-rules] 检测到名字重复(fallback, ${rule.name})，已忽略更新的代理组，这可能导致非预期结果。`,
+        )
+      }
     }
     if (rule.reuse) {
       if (!useable_proxy.includes(rule.name)) useable_proxy.push(rule.name)
